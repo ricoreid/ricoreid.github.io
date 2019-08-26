4ebd51996bcb91bc285bd50778bd0f06470421fb
ricoreid <reid.ricardo@gmail.com>
2019-08-26 10:11:22 +0200

    Just a test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>I'm creating my personal profile</h1>
+      <p>This is where i start, hopefully i'll finish before long.</p>
+    </div>
+  );
+}
+
+export default App;
