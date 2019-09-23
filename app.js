ae23292b7fc9d152dd89cd0324d662c9ac399d55
ricoreid <reid.ricardo@gmail.com>
2019-09-23 22:58:31 +0200

    added some js

---
+++ app.js
@@ -0,0 +1,22 @@
+const navSlide = () => {
+    const berger = document.querySelector('.berger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li')
+
+    berger.addEventListener('click', () =>{
+        // toggle nav
+        nav.classList.toggle('nav-active');
+
+        // animate links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = ''
+            } else{
+                link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s';
+            }
+            console.log(index / 7 + 2);
+        });
+    });
+}
+
+navSlide();
